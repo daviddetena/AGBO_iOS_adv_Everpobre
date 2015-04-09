cae03c4d9267d985a48ee78c338527956fafcf5e
David de Tena <daviddetenacalvo@gmail.com>
2015-04-09 17:43:29 +0200

    Propiedad transient (cosmética) de Photo

--- Everpobre/DTCPhoto.h
+++ Everpobre/DTCPhoto.h
@@ -1,5 +1,9 @@
+@import UIKit;
 #import "_DTCPhoto.h"
 
 @interface DTCPhoto : _DTCPhoto {}
 // Custom logic goes here.
+
+@property (nonatomic,strong) UIImage *image;
+
 @end
--- Everpobre/DTCPhoto.m
+++ Everpobre/DTCPhoto.m
@@ -8,6 +8,16 @@
 
 @implementation DTCPhoto
 
-// Custom logic goes here.
+#pragma mark - Custom properties
+-(UIImage *) image{
+    // Convertimos el NSData de CoreData en UIImage
+    UIImage *image = [UIImage imageWithData:self.photoData];
+    return image;
+}
+
+-(void) setImage:(UIImage *)image{
+    // Convertimos UIImage en NSData entendible por Core Data
+    self.photoData  = UIImageJPEGRepresentation(image, 0.9);
+}
 
 @end
