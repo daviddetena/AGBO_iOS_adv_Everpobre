d15b46d34f465182b53584dc7a74c1b3da9788e7
David de Tena <daviddetenacalvo@gmail.com>
2015-04-10 19:07:07 +0200

    Guardamos en disco al quedar inactiva o entrar en background

--- Everpobre/AppDelegate.m
+++ Everpobre/AppDelegate.m
@@ -27,7 +27,7 @@
     self.stack = [AGTCoreDataStack coreDataStackWithModelName:@"Model"];
     
     // Creamos datos chorras
-    [self createDummyData];
+    //[self createDummyData];
     
     // Creamos el Fetch Request => Búsqueda de todas las libretas
     NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:[DTCNotebook entityName]];
@@ -59,11 +59,21 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    // Guardamos en disco con CoreData
+    [self.stack saveWithErrorBlock:^(NSError *error) {
+        NSLog(@"Error al guardar");
+    }];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // Guardamos en disco con CoreData
+    [self.stack saveWithErrorBlock:^(NSError *error) {
+        NSLog(@"Error al guardar");
+    }];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
