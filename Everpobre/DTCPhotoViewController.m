19798383aaab4379537956a5501606559a6d78ac
David de Tena <daviddetenacalvo@gmail.com>
2015-04-11 23:40:08 +0200

    Filtro vintage

--- Everpobre/DTCPhotoViewController.m
+++ Everpobre/DTCPhotoViewController.m
@@ -8,6 +8,7 @@
 
 #import "DTCPhotoViewController.h"
 #import "DTCPhoto.h"
+@import CoreImage;
 
 @interface DTCPhotoViewController ()
 
@@ -78,6 +79,48 @@
 
 - (IBAction)applyFilter:(id)sender {
     
+    // TAREA: PONER ACTIVITY INDICATOR MIENTRAS SE APLICA EL FILTRO
+    // HACER EL FILTRO EN SEGUNDO PLANO
+    // AL ACABAR, EJECUTAR LA ACTUALIZACIÓN DE LA IMAGEN EN PRIMER
+    // PLANO Y OCULTAR ACTIVITY INDICATOR
+    
+//    // Request a reusable queue and download image in background
+//    dispatch_queue_t download = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    dispatch_async(download, ^{
+//        NSURL *url = [NSURL URLWithString:@"http://media.vandal.net/master/14199/20121210152529_3.jpg"];
+//        NSData *imgData = [NSData dataWithContentsOfURL:url];
+//        
+//        // Completion blocks will be executed in main queue
+//        dispatch_async(dispatch_get_main_queue(), ^{
+//            // Run completion block in main queue
+//            UIImage *image = [UIImage imageWithData:imgData];
+//            completionBlock(image);
+//        });
+//    });
+    
+    // Crear un contexto de CoreImage
+    CIContext *context = [CIContext contextWithOptions:nil];
+    
+    // Imagen de entrada para el filtro, en formato CoreImage: CIImage (necesito una CoreGraphics Image)
+    CIImage *inputImg = [CIImage imageWithCGImage:[self.model.image CGImage]];
+    
+    // Crear filtro y configurarlo mediante KVC
+    CIFilter *vintage = [CIFilter filterWithName:@"CIFalseColor"];
+    [vintage setValue:inputImg forKey:@"inputImage"];
+    
+    // Imagen de salida
+    CIImage *outputImg = vintage.outputImage;
+    
+    // Aplicar filtro (necesitamos CGImageRef)
+    CGImageRef out = [context createCGImage:outputImg fromRect:outputImg.extent];
+    
+    // Sustituyo la imagen
+    self.model.image = [UIImage imageWithCGImage:out];
+    CGImageRelease(out);
+    
+    // Actualizo UI
+    self.photoView.image = self.model.image;
+    
 }
 
 - (IBAction)deletePhoto:(id)sender {
@@ -86,6 +129,8 @@
     
     // AÑADIR EFECTO DE TRANSICIÓN PARA QUE VAYA DESDE EL CENTRO DE LA IMAGEN AL
     // ICONO DE LA PAPELERA
+    // Está en IBOutlet y hay que pedir el centro, que estará en el centro de coordenadas
+    // de la toolbar
     
     // La eliminamos del modelo
     self.model.image = nil;
@@ -117,6 +162,9 @@
     // ¡OJO! PICO DE MEMORIA ASEGURADO, especialmente en dispositivos antiguos (5 o 4s)
     // Sacamos UIImage del diccionario
     UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    // OJO AL DATO
+    // Mirar en el online categoría que hace un resize de la image al tamaño de la pantalla, desapareciendo el pico de memoria
     
     // Guardamos en modelo
     self.model.image = image;
